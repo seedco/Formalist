2188803dc6f525940e7a4b200711908197e4ab7c
Indragie Karunaratne <i@indragie.com>
2016-05-25 18:44:11 -0600

    Add example app target and .gitignore

---
+++ Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Indragie Karunaratne on 2016-05-25.
+//  Copyright © 2016 Seed.co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
