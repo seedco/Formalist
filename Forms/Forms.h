254fdbf8e4f18939f9d5a6e56dcde8415e646cbd
Indragie Karunaratne <i@indragie.com>
2016-05-25 18:42:02 -0600

    Initial Commit

---
+++ Forms/Forms.h
@@ -0,0 +1,19 @@
+//
+//  Forms.h
+//  Forms
+//
+//  Created by Indragie Karunaratne on 2016-05-25.
+//  Copyright © 2016 Seed Platform, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Forms.
+FOUNDATION_EXPORT double FormsVersionNumber;
+
+//! Project version string for Forms.
+FOUNDATION_EXPORT const unsigned char FormsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Forms/PublicHeader.h>
+
+
