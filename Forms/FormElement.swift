6dae0d83b220e20ad0fa62d252214623aee6a578
Indragie Karunaratne <i@indragie.com>
2016-05-25 22:40:22 -0600

    Implement boolean element

---
+++ Forms/FormElement.swift
@@ -0,0 +1,13 @@
+//
+//  FormElement.swift
+//  Forms
+//
+//  Created by Indragie Karunaratne on 3/4/16.
+//  Copyright © 2016 Seed.co. All rights reserved.
+//
+
+import UIKit
+
+public protocol FormElement {
+    func render() -> UIView
+}
