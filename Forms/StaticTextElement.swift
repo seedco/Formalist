4e47934673bf5fd330d0ae9ff423d042b615b025
Indragie Karunaratne <i@indragie.com>
2016-05-27 18:54:34 -0600

    Docs for StaticTextElement

--- Forms/StaticTextElement.swift
+++ Forms/StaticTextElement.swift
@@ -8,12 +8,22 @@
 
 import UIKit
 
+/// An element used to display static text
 public class StaticTextElement: FormElement {
     public typealias ViewConfigurator = UILabel -> Void
     
     private let text: String
     private let viewConfigurator: ViewConfigurator?
     
+    /**
+     Designated initializer
+     
+     - parameter text:             The text to display
+     - parameter viewConfigurator: An optional block used to configure the label
+     used to display the text
+     
+     - returns: An initialized instance of the receiver
+     */
     public init(text: String, viewConfigurator: ViewConfigurator? = nil) {
         self.text = text
         self.viewConfigurator = viewConfigurator
