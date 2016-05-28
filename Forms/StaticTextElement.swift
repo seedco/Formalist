4e4276462183ab7df7b23deef238e3cd8ccaa0d7
Indragie Karunaratne <i@indragie.com>
2016-05-27 18:53:14 -0600

    Implement StaticTextElement

---
+++ Forms/StaticTextElement.swift
@@ -0,0 +1,30 @@
+//
+//  StaticTextElement.swift
+//  Forms
+//
+//  Created by Indragie Karunaratne on 2016-04-26.
+//  Copyright © 2016 Seed Platform, Inc. All rights reserved.
+//
+
+import UIKit
+
+public class StaticTextElement: FormElement {
+    public typealias ViewConfigurator = UILabel -> Void
+    
+    private let text: String
+    private let viewConfigurator: ViewConfigurator?
+    
+    public init(text: String, viewConfigurator: ViewConfigurator? = nil) {
+        self.text = text
+        self.viewConfigurator = viewConfigurator
+    }
+    
+    public func render() -> UIView {
+        let label = UILabel(frame: CGRectZero)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.font = UIFont.preferredFontForTextStyle(UIFontTextStyleBody)
+        label.text = text
+        return label
+    }
+}
