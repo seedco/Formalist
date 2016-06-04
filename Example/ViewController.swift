395f57a256ee980761ca24bc182049a39f636095
Indragie Karunaratne <i@indragie.com>
2016-06-03 22:11:44 -0600

    Set up example app basics

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import Forms
+import StackViewController
 
 class ViewController: UIViewController {
+    private let field1Value = FormValue("")
+    private let field2Value = FormValue("")
+    private let boolValue = FormValue(false)
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        title = "Example"
+        edgesForExtendedLayout = .None
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private lazy var formViewController: FormViewController = {
+        FormViewController(rootElement: GroupElement(style: .Grouped(backgroundColor: .whiteColor()), elements: [
+            BooleanElement(title: "Test Bool", value: self.boolValue),
+            BooleanElement(title: "Test Bool 2", value: self.boolValue),
+        ]))
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        addChildViewController(formViewController)
+        view.addSubview(formViewController.view)
+        formViewController.view.activateSuperviewHuggingConstraints()
+        formViewController.didMoveToParentViewController(self)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
