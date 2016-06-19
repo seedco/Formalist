b669aa6539244b48dc067a36d15fc7562a820873
Indragie Karunaratne <i@indragie.com>
2016-06-18 21:15:18 -0600

    Add convenience initializers for all element types

--- Forms/Convenience.swift
+++ Forms/Convenience.swift
@@ -6,6 +6,148 @@
 //  Copyright © 2016 Seed Platform, Inc. All rights reserved.
 //
 
+/**
+ Creates a form element that displays a control to toggle a boolean value
+ Convenience function for initializing a `BooleanElement`
+ 
+ - parameter title:            The title to display to the toggle
+ - parameter value:            The form value to bind to the toggle
+ - parameter viewConfigurator: An optional closure that can configure the
+ element view, including the title `UILabel` and toggle `UISwitch`
+ 
+ - returns: A boolean element
+ */
+public func toggle(title title: String, value: FormValue<Bool>, viewConfigurator: BooleanElement.ViewConfigurator? = nil) -> BooleanElement {
+    return BooleanElement(title: title, value: value, viewConfigurator: viewConfigurator)
+}
+
+/**
+ Creates a form element that displays a segmented control
+ Convenience function for initializing a `SegmentElement`
+ 
+ - parameter title:            The title to display above the segmented control
+ - parameter segments:         The segments to display in the segmented control
+ - parameter selectedValue:    The value to bind to the value of the selected
+ segment
+ - parameter viewConfigurator: An optional closure for configuring the appearance
+ of the view, including the title label and segmented control
+ 
+ - returns: A segment element
+ */
+public func segments<ValueType: Equatable>(title title: String,
+                                                 segments: [Segment<ValueType>],
+                                                 selectedValue: FormValue<ValueType>,
+                                                 viewConfigurator: SegmentElement<ValueType>.ViewConfigurator? = nil)
+    -> SegmentElement<ValueType> {
+    return SegmentElement(
+        title: title,
+        segments: segments,
+        selectedValue: selectedValue,
+        viewConfigurator: viewConfigurator
+    )
+}
+
+/**
+ Creates a form element that displays static text.
+ Convenience function for initializing a `StaticTextElement`
+ 
+ - parameter text:             The text to display
+ - parameter viewConfigurator: An optional block used to configure the appearance
+ of the view
+ 
+ - returns: A static text form element
+ */
+public func staticText(text: String, viewConfigurator: StaticTextElement.ViewConfigurator? = nil) -> StaticTextElement {
+    return StaticTextElement(text: text, viewConfigurator: viewConfigurator)
+}
+
+/**
+ Creates a form element that displays a cell that invokes an action when tapped
+ Convenience function for initializing a `SegueElement`
+ 
+ - parameter icon:             An optional icon to display
+ - parameter title:            The title to display
+ - parameter viewConfigurator: An optional block used to configure the appearance
+ of the view
+ - parameter action:           The action to invoke when the view is tapped
+ 
+ - returns: A segue element
+ */
+public func segue(icon icon: UIImage?,
+                       title: String,
+                       viewConfigurator: SegueElement.ViewConfigurator? = nil,
+                       action: Void -> Void) -> SegueElement {
+    return SegueElement(
+        icon: icon,
+        title: title,
+        viewConfigurator: viewConfigurator,
+        action: action
+    )
+}
+
+/**
+ Creates a form element that displays a fixed height spacer
+ Convenience function for initializing a `SpacerElement`
+ 
+ - parameter height:           The height of the spacer
+ - parameter viewConfigurator: An optional block used to configure the
+ appearance of the spacer view
+ 
+ - returns: A spacer element
+ */
+public func spacer(height height: CGFloat, viewConfigurator: SpacerElement.ViewConfigurator? = nil) -> SpacerElement {
+    return SpacerElement(height: height, viewConfigurator: viewConfigurator)
+}
+
+/**
+ Creates a form element that wraps an existing view
+ Convenience function for initializing a `ViewElement`
+ 
+ - parameter value:            The value to bind to the element
+ - parameter viewConfigurator: A block that creates and configures
+ a view associated with the element
+ 
+ - returns: A view element
+ */
+public func customView<ValueType: Equatable>(value value: FormValue<ValueType>, viewConfigurator: ViewElement<ValueType>.ViewConfigurator) -> ViewElement<ValueType> {
+    return ViewElement(value: value, viewConfigurator: viewConfigurator)
+}
+
+/**
+ Creates a form element that groups child elements
+ Convenience function for initializing a `GroupElement`
+ 
+ - parameter configuration: The configuration parameters to use. See the
+ documentation for the `Configuration` struct.
+ - parameter elements:      The elements to group
+ 
+ - returns: A group element
+ */
+public func group(configuration configuration: GroupElement.Configuration = GroupElement.Configuration(), elements: [FormElement]) -> GroupElement {
+    return GroupElement(configuration: configuration, elements: elements)
+}
+
+/**
+ Creates a form element that groups child elements
+ Convenience function for initializing a `GroupElement`
+ 
+ - parameter configurator: Block used to set up configuration properties
+ - parameter elements:     The elements to group
+ 
+ - returns: A group element
+ */
+public func group(configurator configurator: (inout GroupElement.Configuration) -> Void, elements: [FormElement]) -> GroupElement {
+    return GroupElement(configurator: configurator, elements: elements)
+}
+
+/**
+ Creates a group element that insets the specified child elements
+ 
+ - parameter insets:   The insets to apply to the child elements
+ - parameter elements: The child elements to inset
+ 
+ - returns: A group element
+ */
 public func inset(insets: UIEdgeInsets, elements: [FormElement]) -> GroupElement {
     return GroupElement(configurator: {
         $0.layout.edgeInsets = insets
@@ -127,7 +269,7 @@ public func singleLineFloatLabel(name name: String,
  - parameter viewConfigurator: An optional block used to perform additional
  customization of the float label
  
- - returns: An editable text elemen
+ - returns: An editable text element
  */
 public func multiLineFloatLabel(name name: String,
                                      value: FormValue<String>,
