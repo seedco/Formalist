0332d4af8a7bffc1cca5bda23a9cd3b52800c70a
Viktor Radchenko <yazexel@gmail.com>
2017-03-24 12:41:15 -0700

    Added accessoryIcon to SegueElement

--- Formalist/SegueElementView.swift
+++ Formalist/SegueElementView.swift
@@ -20,8 +20,11 @@ open class SegueElementView: UIView {
     
     /// The image view used to display the icon
     open let imageView: UIImageView
+
+    /// The button used to display the right icon
+    open let accessoryButton: UIButton
     
-    init(icon: UIImage?, title: String) {
+    init(icon: UIImage?, title: String, accessoryIcon: UIImage?) {
         label = UILabel(frame: CGRect.zero)
         label.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
         label.text = title
@@ -29,10 +32,15 @@ open class SegueElementView: UIView {
         imageView = UIImageView(image: icon)
         imageView.isHidden = (icon == nil)
         imageView.setContentHuggingPriority(UILayoutPriorityDefaultHigh, for: .horizontal)
-        
+
+        accessoryButton = UIButton(type: .custom)
+        accessoryButton.setImage(accessoryIcon, for: .normal)
+        accessoryButton.isHidden = (accessoryIcon == nil)
+        accessoryButton.setContentHuggingPriority(UILayoutPriorityDefaultHigh, for: .horizontal)
+
         super.init(frame: CGRect.zero)
         
-        let stackView = UIStackView(arrangedSubviews: [imageView, label])
+        let stackView = UIStackView(arrangedSubviews: [imageView, label, accessoryButton])
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .horizontal
         stackView.alignment = .center
