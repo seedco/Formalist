f5bccae330fd71d73ccd761b8633b4fc111b0209
Viktor Radchenko <yazexel@gmail.com>
2017-07-22 00:38:02 -0700

    Add Formattable

---
+++ Formalist/Formattable.swift
@@ -0,0 +1,13 @@
+//
+//  Formattable.swift
+//  Formalist
+//
+//  Created by Viktor Radchenko on 7/21/17.
+//  Copyright © 2017 Seed Platform, Inc. All rights reserved.
+//
+
+import Foundation
+
+public protocol Formattable {
+    func from(input: String, previousInput: String) -> String
+}
