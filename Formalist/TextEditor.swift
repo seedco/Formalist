b33ab9f35a238c9811fea6ff24857f6aaa77e3a2
Klaas Pieter Annema <kpa@annema.me>
2018-05-28 17:10:27 +0200

    Abstract presenting accessory toolbar

---
+++ Formalist/TextEditor.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+protocol TextEditor: class {
+    var nextFormResponder: UIView? { get }
+
+    var inputAccessoryView: UIView? { get set }
+
+    @discardableResult func resignFirstResponder() -> Bool
+}
+
+func presentToolbar(with editor: TextEditor, configuration: TextEditorConfiguration) {
+    let toolbar = AccessoryViewToolbar(
+        frame: .zero,
+        doneButtonCustomTitle: configuration.doneButtonCustomTitle
+    )
+    toolbar.callbacks = AccessoryViewToolbarCallbacks(
+        nextAction: { [weak editor] in
+            editor?.nextFormResponder?.becomeFirstResponder()
+            configuration.textEditorAction?(.next)
+        },
+        doneAction: { [weak editor] in
+            editor?.resignFirstResponder()
+            configuration.textEditorAction?(.done)
+        }
+    )
+    toolbar.sizeToFit()
+    editor.inputAccessoryView = toolbar
+
+    // Hide next button when nextFormResponder == nil.
+    if editor.nextFormResponder == nil {
+        toolbar.nextButtonItem.isEnabled = false
+        toolbar.nextButtonItem.title = ""
+    }
+}
